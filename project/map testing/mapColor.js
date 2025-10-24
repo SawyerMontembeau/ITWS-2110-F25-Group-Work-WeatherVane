10d01effc31f811348129517d1663ef83c4a003e
Katrina Mosquera-Maykovych <katrinamosquera@gmail.com>
2025-10-23 20:46:51 -0400

    Alt Name Reference Dictionary is Working as Intended
    Since there are discrepancies between the names of regions in sri_lanka.json and lk.json, the only way to reconcile these discrepancies was to create a dictionary correlating each region name in sri_lanka.json to its corresponding label in lk.json.
    
    After minimal testing, this appears to be a working solution.

--- project/map testing/mapColor.js
+++ project/map testing/mapColor.js
@@ -1,5 +1,36 @@
+const regionNames = {};
+regionNames.Ampara = "Ampāra";
+regionNames.Anuradhapura = "Anurādhapura";
+regionNames.Badulla = "Badulla";
+regionNames.Batticaloa = "Maḍakalapuva";
+regionNames.Colombo = "Kŏḷamba";
+regionNames.Galle = "Gālla";
+regionNames.Gampaha = "Gampaha";
+regionNames.Hambantota = "Hambantŏṭa";
+regionNames.Jaffna = "Yāpanaya";
+regionNames.Kalutara = "Kaḷutara";
+regionNames.Kandy = "Mahanuvara";
+regionNames.Kegalle = "Kægalla";
+regionNames.Kilinochchi = "Kilinŏchchi";
+regionNames.Kurunegala = "Kuruṇægala";
+regionNames.Mannar = "Mannārama";
+regionNames.Matale = "Mātale";
+regionNames.Matara = "Mātara";
+regionNames.Moneragala = "Mŏṇarāgala";
+regionNames.Mullaitivu = "Mulativ";
+regionNames.NuwaraEliya = "Nuvara Ĕliya";
+regionNames.Polonnaruwa = "Pŏḷŏnnaruva";
+regionNames.Puttalam = "Puttalama";
+regionNames.Ratnapura = "Ratnapura";
+regionNames.Trincomalee = "Trikuṇāmalaya";
+regionNames.Vavuniya = "Vavuniyāva";
+
 document.addEventListener('mapReady', (event) => {
   const { regions } = event.detail;
-
+  console.log("Hello!");
+  for(const region in regionNames) {
+    console.log(regionNames[region]);
+    regions[regionNames[region]].setStyle({color: '#FF0000'});
+  }
   regions['Mulativ'].setStyle({color: '#FF0000'});
 });
